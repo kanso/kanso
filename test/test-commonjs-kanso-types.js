2d35cff29741d7a3cd9cc37924027feef5105c49
Caolan McMahon <caolan@caolanmcmahon.com>
2011-02-18 14:17:49 +0000

    add test for running fields validators in nested objects

--- test/test-commonjs-kanso-types.js
+++ test/test-commonjs-kanso-types.js
@@ -69,6 +69,39 @@ module.exports = nodeunit.testCase({
         test.same(errs.length, 1);
         test.same(errs[0].message, 'Not a number');
         test.done();
+    },
+
+    'validate - nested': function (test) {
+        test.expect(5);
+        var types = this.types;
+        var fields = this.fields;
+
+        var testdoc = {
+            type: 'test_type',
+            one: 'one',
+            sublevel: {
+                two: 2,
+                three: true
+            }
+        };
+        var type_fields = {
+            one: fields.string(),
+            sublevel: {
+                two: fields.number(),
+                three: fields.boolean()
+            }
+        }
+        type_fields.sublevel.three.validators.push(function (doc, value, raw) {
+            test.ok(true, 'sublevel validators called');
+            test.same(doc, testdoc);
+            test.same(value, true);
+            test.same(raw, true);
+        });
+        var errs = types.validate({
+            'test_type': {fields: type_fields}
+        }, testdoc);
+        test.same(errs, []);
+        test.done();
     }
 
 });
