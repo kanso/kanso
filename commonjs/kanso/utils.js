83afaca70f2332a5b674ade74cc4d25b57ab9e48
Caolan McMahon <caolan@caolanmcmahon.com>
2011-03-14 19:11:17 +0000

    remove clone function from utils

--- commonjs/kanso/utils.js
+++ commonjs/kanso/utils.js
@@ -100,19 +100,3 @@ exports.setPropertyPath = function (obj, path, val) {
     }
     exports.setPropertyPath(obj[next], path, val);
 };
-
-/**
- * Makes a shallow-copy of an object and returns it, sub objects and arrays will
- * not be duplicated just copied by reference.
- */
-
-exports.clone = function (obj) {
-    if (exports.isArray(obj)) {
-        return obj.slice();
-    }
-    var result = {};
-    for (var k in obj) {
-        result[k] = obj[k];
-    }
-    return result;
-};
