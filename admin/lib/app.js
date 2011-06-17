b815bacbfc49bbd7503b5ec1a5a63e152cab7266
Caolan McMahon <caolan@caolanmcmahon.com>
2011-06-16 17:36:43 -0700

    update rewrite query params to be strings to work around bug in CouchDB 1.1 rewriter #131

--- admin/lib/app.js
+++ admin/lib/app.js
@@ -19,15 +19,15 @@ exports.rewrites = [
     {from: '/:app/types/:type', to: '_list/typelist/types', query: {
         startkey: [':type'],
         endkey: [':type', {}],
-        include_docs: true,
-        limit: 10
+        include_docs: 'true',
+        limit: '10'
     }},
     {from: '/:app/view/:id', to: '_show/viewtype/:id', method: 'GET'},
     {from: '/:app/edit/:id', to: '_show/edittype/:id', method: 'GET'},
     {from: '/:app/edit/:id', to: '_update/updatetype/:id', method: 'POST'},
     {from: '/:app/delete/:id', to: '_update/deletetype/:id', method: 'POST'},
     {from: '/:app/views/:view', to: '_show/viewlist', query: {
-        limit: 10
+        limit: '10'
     }}
 ];
 
