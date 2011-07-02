de62234ed266d9c3e0b16b9deafa7311458e1e6d
Caolan McMahon <caolan@caolanmcmahon.com>
2011-07-01 17:53:09 -0700

    specify an ordered list of builtin plugins rather than reading the plugins directory

--- test/test-lib-plugins.js
+++ test/test-lib-plugins.js
@@ -1,45 +1,47 @@
-var plugins = require('../lib/plugins');
+var plugins = require('../lib/plugins'),
+    nodeunit = require('../deps/nodeunit');
 
 
-exports['commands'] = function (test) {
-    var paths = [
-        __dirname + '/fixtures/plugins/one',
-        __dirname + '/fixtures/plugins/noconflict_two'
-    ];
-    var p = plugins.load(paths);
-    test.same(Object.keys(plugins.commands(p)), ['foo', 'bar', 'baz', 'qux']);
-    test.done();
-};
+exports.plugins = nodeunit.testCase({
 
-exports['detect conflicting command names'] = function (test) {
-    test.expect(1);
-    var paths = [
-        __dirname + '/fixtures/plugins/one',
-        __dirname + '/fixtures/plugins/conflict_two'
-    ];
-    try {
+    setUp: function (cb) {
+        this._BUILTIN = plugins.BUILTIN;
+        plugins.BUILTIN = [];
+        cb();
+    },
+
+    tearDown: function (cb) {
+        plugins.BUILTIN = this._BUILTIN;
+        cb();
+    },
+
+    'commands': function (test) {
+        var paths = [
+            __dirname + '/fixtures/plugins/one',
+            __dirname + '/fixtures/plugins/noconflict_two'
+        ];
         var p = plugins.load(paths);
-    }
-    catch (e) {
-        test.equal(
-            e.message,
-            'Command "bar" in "' + paths[0] + '" conflicts with "' +
-            paths[1] + '"'
-        );
-    }
-    test.done();
-};
-
-exports['list plugins in dir'] = function (test) {
-    plugins.list(__dirname + '/fixtures/plugins', function (err, plugins) {
-        if (err) {
-            return test.done(err);
-        }
-        test.same(plugins.sort(), [
-            __dirname + '/fixtures/plugins/conflict_two',
-            __dirname + '/fixtures/plugins/noconflict_two',
-            __dirname + '/fixtures/plugins/one'
-        ]);
+        test.same(Object.keys(plugins.commands(p)), ['foo', 'bar', 'baz', 'qux']);
         test.done();
-    });
-};
+    },
+
+    'detect conflicting command names': function (test) {
+        test.expect(1);
+        var paths = [
+            __dirname + '/fixtures/plugins/one',
+            __dirname + '/fixtures/plugins/conflict_two'
+        ];
+        try {
+            var p = plugins.load(paths);
+        }
+        catch (e) {
+            test.equal(
+                e.message,
+                'Command "bar" in "' + paths[0] + '" conflicts with "' +
+                paths[1] + '"'
+            );
+        }
+        test.done();
+    }
+
+});
