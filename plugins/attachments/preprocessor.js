09090c9c252514de824d79f03087a8717098e6af
Caolan McMahon <caolan@caolanmcmahon.com>
2011-07-04 16:09:00 -0700

    added attachments module and plugin

--- plugins/attachments/preprocessor.js
+++ plugins/attachments/preprocessor.js
@@ -1,19 +1,19 @@
-var modules = require('../../lib/modules'),
+var attachments = require('../../lib/attachments'),
     async = require('../../deps/async');
 
 
 /**
- * Loads module directories specified in kanso.json and adds the modules
+ * Loads attachment directories specified in kanso.json and adds the attachments
  * to the document.
  */
 
 module.exports = function (path, settings, doc, callback) {
-    var paths = settings.modules || [];
+    var paths = settings.attachments || [];
     if (!Array.isArray(paths)) {
         paths = [paths];
     }
     async.forEach(paths, function (p, cb) {
-        modules.addPath(path, p, doc, cb);
+        attachments.addPath(path, p, doc, cb);
     },
     function (err) {
         callback(err, doc);
