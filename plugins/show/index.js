2f4cd1f8a14dc48570f893ff4228cbf455459e5f
Caolan McMahon <caolan@caolanmcmahon.com>
2011-07-04 08:47:48 -0700

    added show command plugin

---
+++ plugins/show/index.js
@@ -0,0 +1,3 @@
+exports.commands = {
+    'show': require('./show')
+};
---
+++ plugins/show/show.js
@@ -0,0 +1,25 @@
+var utils = require('../../lib/utils'),
+    packages = require('../../lib/packages'),
+    logger = require('../../lib/logger');
+
+
+exports.summary = 'Load a project and output resulting JSON';
+
+exports.usage = '' +
+'kanso show [PATH]\n' +
+'\n' +
+'Parameters:\n' +
+'  PATH     Path to project directory to show (defaults to ".")';
+
+
+exports.run = function (settings, plugins, args) {
+    var dir = utils.abspath(args[0] || '.');
+    packages.load(plugins, dir, [], null, function (err, doc) {
+        if (err) {
+            return logger.error(err);
+        }
+        // output to console (not a log entry, actual output)
+        console.log(JSON.stringify(doc, null, 4));
+        logger.clean_exit = true;
+    });
+};
