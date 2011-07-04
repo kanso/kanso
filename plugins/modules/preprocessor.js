370802fb030c8f69c49513a2f99aa469eb8ba79c
Caolan McMahon <caolan@caolanmcmahon.com>
2011-07-04 14:32:42 -0700

    add some comments to plugin preprocessors

--- plugins/modules/preprocessor.js
+++ plugins/modules/preprocessor.js
@@ -2,6 +2,11 @@ var modules = require('../../lib/modules'),
     async = require('../../deps/async');
 
 
+/**
+ * Loads module directories specified in kanso.json and adds the modules
+ * to the document.
+ */
+
 module.exports = function (path, settings, doc, callback) {
     var paths = settings.modules || [];
     async.forEach(paths, function (p, cb) {
--- plugins/settings/preprocessor.js
+++ plugins/settings/preprocessor.js
@@ -1,5 +1,9 @@
 var modules = require('../../lib/modules');
 
+/**
+ * Wraps the settings JSON as a CommonJS Module and includes in the document
+ * so package config is easily available in the CommonJS environment.
+ */
 
 module.exports = function (path, settings, doc, callback) {
     if (!doc.settings) {
