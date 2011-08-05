a7db600fcb1f81c15bbcfc9cc07c76cee6338b98
Caolan McMahon <caolan@caolanmcmahon.com>
2011-08-05 10:48:36 -0700

    add postprocessor to kanso.types package which removes type definitions added to ddoc by properties package

---
+++ packages/kanso.types/build/remove_property.js
@@ -0,0 +1,13 @@
+/**
+ * Removes the types property from the design doc, since the admin app expects
+ * the types to be exported from the root module (meaning they'll get copied to
+ * the design doc by the properties package).
+ */
+
+module.exports = {
+    after: 'properties',
+    run: function (path, settings, doc, callback) {
+        delete doc.types;
+        callback(null, doc);
+    }
+};
