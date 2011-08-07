ef213e49cab59ebc83ac16071464778ad5fab162
Caolan McMahon <caolan@caolanmcmahon.com>
2011-08-07 11:23:59 -0700

    add minify option to modules postprocessor

--- packages/modules/build/attachment.js
+++ packages/modules/build/attachment.js
@@ -1,8 +1,18 @@
 var fs = require('fs'),
     mime = require('../../../deps/node-mime/mime'),
+    jsp = require('../../../deps/UglifyJS/lib/parse-js'),
+    pro = require('../../../deps/UglifyJS/lib/process'),
     modules = require('../../../lib/modules'),
     logger = require('../../../lib/logger'),
     utils = require('../../../lib/utils');
+
+
+function minify(src) {
+    var ast = jsp.parse(src);   // parse code and get the initial AST
+    ast = pro.ast_mangle(ast);  // get a new AST with mangled names
+    ast = pro.ast_squeeze(ast); // get an AST with compression optimizations
+    return pro.gen_code(ast);   // compressed code here
+}
 
 
 /**
