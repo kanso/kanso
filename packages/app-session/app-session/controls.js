f70a44ea52690adbed90dd6396c959a2dc64d2a1
Caolan McMahon <caolan@caolanmcmahon.com>
2011-08-21 23:25:22 -0700

    refactor login form controls code

--- packages/app-session/app-session/controls.js
+++ packages/app-session/app-session/controls.js
@@ -1,6 +1,35 @@
 var session = require('kanso/session');
 
 
+exports.clearErrors = function (form) {
+    $('.general_errors', form).text('');
+    $('.username .errors', form).text('');
+    $('.username').removeClass('validation_error');
+    $('.password .errors', form).text('');
+    $('.password').removeClass('validation_error');
+};
+
+exports.showDropDown = function () {
+    $('#session .login').addClass('active');
+    $('#login_dropdown').show();
+    $('#login_dropdown input[name="name"]').focus();
+};
+
+exports.hideDropDown = function () {
+    $('#session .login').removeClass('active');
+    $('#login_dropdown').hide();
+    exports.clearErrors();
+};
+
+exports.toggleDropDown = function () {
+    if ($('#session .login').hasClass('active')) {
+        exports.hideDropDown();
+    }
+    else {
+        exports.showDropDown();
+    }
+};
+
 exports.bind = function () {
     $('#session .logout a').click(function (ev) {
         ev.preventDefault();
@@ -9,37 +38,26 @@ exports.bind = function () {
     });
     $('#session .login a').click(function (ev) {
         ev.preventDefault();
-        $('#session .login').addClass('active');
-        $('#login_dropdown').show();
-        $('#login_dropdown input[name="name"]').focus();
+        exports.toggleDropDown();
         return false;
     });
     $('#login_form_cancel').click(function (ev) {
-        $('#session .login').removeClass('active');
-        $('#login_dropdown').hide();
+        exports.hideDropDown();
     });
     $('#login_form').submit(function (ev) {
         ev.preventDefault();
         var form = this;
-        $('.general_errors', form).text('');
         var spinner_elt = $('.spinner', form).show();
         var username = $('input[name="name"]', form).val();
         var password = $('input[name="password"]', form).val();
+        exports.clearErrors(form);
         if (!username) {
             $('.username .errors', form).text('Please enter a username');
             $('.username').addClass('validation_error');
         }
-        else {
-            $('.username .errors', form).text('');
-            $('.username').removeClass('validation_error');
-        }
         if (!password) {
             $('.password .errors', form).text('Please enter a password');
             $('.password').addClass('validation_error');
-        }
-        else {
-            $('.password .errors', form).text('');
-            $('.password').removeClass('validation_error');
         }
         if (username && password) {
             session.login(username, password, function (err) {
