feb12532c96b1c57493657af562112d2454d5759
Caolan McMahon <caolan@caolanmcmahon.com>
2011-08-30 09:56:26 -0700

    added less-precompiler package and some extra info to attachments before pushing

---
+++ packages/attachments/build/cleanup.js
@@ -0,0 +1,18 @@
+var attachments = require('../../../lib/attachments'),
+    async = require('../../../deps/async');
+
+
+/**
+ * Loads attachment directories specified in kanso.json and adds the attachments
+ * to the document.
+ */
+
+module.exports = function (root, path, settings, doc, callback) {
+    if (!doc._attachments) {
+        return callback(null, doc);
+    }
+    for (var k in doc._attachments) {
+        delete doc._attachments[k]._original_path;
+    }
+    callback(null, doc);
+};
