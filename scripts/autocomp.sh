c55d032caaba3de3ca5d40c993a767d520b32608
Jason Smith (air) <jhs@iriscouch.com>
2011-12-20 09:03:47 +0700

    Remove NODE_PATH and KANSO_DIR from the autocompletion script

--- scripts/autocomp.sh
+++ scripts/autocomp.sh
@@ -1,17 +1,17 @@
+# Remember the absolute path to the autocompleter.
+_kanso_autocomp_js=$(dirname "$BASH_SOURCE")/autocomp.js
+if [ `uname` = "Darwin" ]; then
+    _kanso_autocomp_js=$(ruby -e "puts File.expand_path('$_kanso_autocomp_js')")
+else
+    _kanso_autocomp_js=$(readlink -f "$_kanso_autocomp_js")
+fi
+
 _compListener() {
-  # Additional path for looking up node modules.
-  # Used so kanso package pre/postprocessors can do require('kanso/lib/foo')
-  # KANSO_DIR=/usr/local/lib/node/kanso
-  # ^ this is now added by Makefile
-
-  # Extend existing NODE_PATH environment variable
-  export NODE_PATH="$KANSO_DIR/src:$KANSO_DIR/deps:$NODE_PATH"
-
   local curw
   COMPREPLY=()
   curw=${COMP_WORDS[COMP_CWORD]}
-  # TODO: set autocomp.js path in Makefile
-  COMPREPLY=($($KANSO_DIR/scripts/autocomp.js ${COMP_WORDS[@]}))
+
+  COMPREPLY=($($_kanso_autocomp_js ${COMP_WORDS[@]}))
   return 0
 }
 #complete -F _compListener -o nospace kanso
