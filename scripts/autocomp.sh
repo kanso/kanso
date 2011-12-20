59438f711b9ebce5b3d76c82bda7c0768388eeec
Jason Smith (air) <jhs@iriscouch.com>
2011-12-20 10:45:36 +0700

    Let NPM install autocomp.js and call it from the completion function

--- scripts/autocomp.sh
+++ scripts/autocomp.sh
@@ -1,17 +1,9 @@
-# Remember the absolute path to the autocompleter.
-_kanso_autocomp_js=$(dirname "$BASH_SOURCE")/autocomp.js
-if [ `uname` = "Darwin" ]; then
-    _kanso_autocomp_js=$(ruby -e "puts File.expand_path('$_kanso_autocomp_js')")
-else
-    _kanso_autocomp_js=$(readlink -f "$_kanso_autocomp_js")
-fi
-
 _compListener() {
   local curw
   COMPREPLY=()
   curw=${COMP_WORDS[COMP_CWORD]}
 
-  COMPREPLY=($($_kanso_autocomp_js ${COMP_WORDS[@]}))
+  COMPREPLY=($(kanso_completions ${COMP_WORDS[@]}))
   return 0
 }
 #complete -F _compListener -o nospace kanso
