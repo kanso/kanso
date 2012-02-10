777a79e8381be65b142546ac1ba46509eacd75fb
Caolan McMahon <caolan@caolanmcmahon.com>
2012-02-09 16:42:01 -0800

    add support for tree builder to get more avilable versions from sources

--- test/test-lib-tree.js
+++ test/test-lib-tree.js
@@ -1,7 +1,7 @@
 var tree = require('../lib/tree');
 
 
-exports['build - install new - reduce dep version'] = function (test) {
+exports['extend - install new - reduce dep version'] = function (test) {
     var packages = {
         'foo': {
             versions: {
@@ -22,35 +22,37 @@ exports['build - install new - reduce dep version'] = function (test) {
         }
     }
     var sources = [];
-    tree.build(bar, sources, packages);
-    test.same(packages, {
-        'foo': {
-            versions: {
-                '0.0.1': { name: 'foo', version: '0.0.1', dependencies: {} },
-                '0.0.2': { name: 'foo', version: '0.0.2', dependencies: {} },
-                '0.0.3': { name: 'foo', version: '0.0.3', dependencies: {} }
+    tree.extend(bar, sources, packages, function (err, packages) {
+        test.same(packages, {
+            'foo': {
+                versions: {
+                    '0.0.1': { name: 'foo', version: '0.0.1', dependencies: {} },
+                    '0.0.2': { name: 'foo', version: '0.0.2', dependencies: {} },
+                    '0.0.3': { name: 'foo', version: '0.0.3', dependencies: {} }
+                },
+                current_version: '0.0.2',
+                ranges: {'bar': '<= 0.0.2'},
+                sources: []
             },
-            current_version: '0.0.2',
-            ranges: {'bar': '<= 0.0.2'},
-            sources: []
-        },
-        'bar': {
-            versions: {
-                '0.0.1': {
-                    name: 'bar',
-                    version: '0.0.1',
-                    dependencies: { 'foo': '<= 0.0.2' }
-                }
-            },
-            current_version: '0.0.1',
-            ranges: {},
-            sources: []
-        }
+            'bar': {
+                versions: {
+                    '0.0.1': {
+                        name: 'bar',
+                        version: '0.0.1',
+                        dependencies: { 'foo': '<= 0.0.2' }
+                    }
+                },
+                current_version: '0.0.1',
+                ranges: {},
+                sources: []
+            }
+        });
+        test.done(err);
     });
-    test.done();
 };
 
-exports['build - reinstall existing - increase dep version'] = function (test) {
+
+exports['extend - reinstall existing - increase dep version'] = function (test) {
     var packages = {
         'foo': {
             versions: {
@@ -83,40 +85,42 @@ exports['build - reinstall existing - increase dep version'] = function (test) {
         }
     }
     var sources = [];
-    tree.build(bar, sources, packages);
-    test.same(packages, {
-        'foo': {
-            versions: {
-                '0.0.1': { name: 'foo', version: '0.0.1', dependencies: {} },
-                '0.0.2': { name: 'foo', version: '0.0.2', dependencies: {} },
-                '0.0.3': { name: 'foo', version: '0.0.3', dependencies: {} }
-            },
-            current_version: '0.0.3',
-            ranges: {bar: '> 0.0.2'},
-            sources: []
-        },
-        'bar': {
-            versions: {
-                '0.0.1': {
-                    name: 'bar',
-                    version: '0.0.1',
-                    dependencies: { 'foo': '<= 0.0.2' }
+    tree.extend(bar, sources, packages, function (err, packages) {
+        test.same(packages, {
+            'foo': {
+                versions: {
+                    '0.0.1': { name: 'foo', version: '0.0.1', dependencies: {} },
+                    '0.0.2': { name: 'foo', version: '0.0.2', dependencies: {} },
+                    '0.0.3': { name: 'foo', version: '0.0.3', dependencies: {} }
                 },
-                '0.0.2': {
-                    name: 'bar',
-                    version: '0.0.2',
-                    dependencies: { 'foo': '> 0.0.2' }
-                }
+                current_version: '0.0.3',
+                ranges: {bar: '> 0.0.2'},
+                sources: []
             },
-            current_version: '0.0.2',
-            ranges: {},
-            sources: []
-        }
+            'bar': {
+                versions: {
+                    '0.0.1': {
+                        name: 'bar',
+                        version: '0.0.1',
+                        dependencies: { 'foo': '<= 0.0.2' }
+                    },
+                    '0.0.2': {
+                        name: 'bar',
+                        version: '0.0.2',
+                        dependencies: { 'foo': '> 0.0.2' }
+                    }
+                },
+                current_version: '0.0.2',
+                ranges: {},
+                sources: []
+            }
+        });
+        test.done(err);
     });
-    test.done();
 };
 
-exports['build - install new - missing dep version'] = function (test) {
+
+exports['extend- install new - missing dep version'] = function (test) {
     test.expect(1);
     var packages = {
         'foo': {
@@ -137,19 +141,17 @@ exports['build - install new - missing dep version'] = function (test) {
         }
     }
     var sources = [];
-    try {
-        tree.build(bar, sources, packages);
-    }
-    catch (e) {
+    tree.extend(bar, sources, packages, function (err, packages) {
         test.ok(
-            /No matching version for 'foo'/.test(e.message),
+            /No matching version for 'foo'/.test(err.message),
             "No matching version for 'foo'"
         );
-    }
-    test.done();
+        test.done();
+    });
 };
 
-exports['build - install new - missing dep package'] = function (test) {
+
+exports['extend - install new - missing dep package'] = function (test) {
     test.expect(1);
     var packages = {
         'foo': {
@@ -170,14 +172,248 @@ exports['build - install new - missing dep package'] = function (test) {
         }
     }
     var sources = [];
-    try {
-        tree.build(bar, sources, packages);
-    }
-    catch (e) {
+    tree.extend(bar, sources, packages, function (err, packages) {
         test.ok(
-            /No package for 'baz'/.test(e.message),
+            /No package for 'baz'/.test(err.message),
             "No package for 'baz'"
         );
-    }
-    test.done();
+        test.done();
+    });
+};
+
+
+exports['build - fetch from sources'] = function (test) {
+    test.expect(2);
+    var foo = {
+        name: 'foo',
+        version: '0.0.1',
+        dependencies: {
+            'bar': '>= 0.0.2'
+        }
+    };
+    var sources = [
+        function (name, callback) {
+            test.equal(name, 'bar');
+            process.nextTick(function () {
+                callback(null, {
+                    '0.0.1': {
+                        name: 'bar',
+                        version: '0.0.1',
+                        dependencies: {}
+                    },
+                    '0.0.2': {
+                        name: 'bar',
+                        version: '0.0.2',
+                        dependencies: {}
+                    }
+                });
+            })
+        }
+    ];
+    tree.build(foo, sources, function (err, packages) {
+        test.same(packages, {
+            'foo': {
+                versions: {
+                    '0.0.1': {
+                        name: 'foo',
+                        version: '0.0.1',
+                        dependencies: {
+                            'bar': '>= 0.0.2'
+                        }
+                    }
+                },
+                ranges: {},
+                sources: [],
+                current_version: '0.0.1'
+            },
+            'bar': {
+                versions: {
+                    '0.0.1': {
+                        name: 'bar',
+                        version: '0.0.1',
+                        dependencies: {}
+                    },
+                    '0.0.2': {
+                        name: 'bar',
+                        version: '0.0.2',
+                        dependencies: {}
+                    }
+                },
+                ranges: {'foo': '>= 0.0.2'},
+                sources: [],
+                current_version: '0.0.2'
+            }
+        });
+        test.done(err);
+    });
+};
+
+
+exports['build - check multiple sources'] = function (test) {
+    test.expect(4);
+    var source_calls = [];
+    var foo = {
+        name: 'foo',
+        version: '0.0.1',
+        dependencies: {
+            'bar': '>= 0.0.2'
+        }
+    };
+    var sources = [
+        function (name, callback) {
+            test.equal(name, 'bar');
+            source_calls.push('one');
+            process.nextTick(function () {
+                callback(null, {
+                    '0.0.1': {
+                        name: 'bar',
+                        version: '0.0.1',
+                        dependencies: {}
+                    }
+                });
+            })
+        },
+        function (name, callback) {
+            test.equal(name, 'bar');
+            source_calls.push('two');
+            process.nextTick(function () {
+                callback(null, {
+                    '0.0.1': {
+                        name: 'bar',
+                        version: '0.0.1',
+                        dependencies: {}
+                    },
+                    '0.0.2': {
+                        name: 'bar',
+                        version: '0.0.2',
+                        dependencies: {}
+                    }
+                });
+            })
+        }
+    ];
+    tree.build(foo, sources, function (err, packages) {
+        test.same(source_calls, ['one', 'two']);
+        test.same(packages, {
+            'foo': {
+                versions: {
+                    '0.0.1': {
+                        name: 'foo',
+                        version: '0.0.1',
+                        dependencies: {
+                            'bar': '>= 0.0.2'
+                        }
+                    }
+                },
+                ranges: {},
+                sources: [],
+                current_version: '0.0.1'
+            },
+            'bar': {
+                versions: {
+                    '0.0.1': {
+                        name: 'bar',
+                        version: '0.0.1',
+                        dependencies: {}
+                    },
+                    '0.0.2': {
+                        name: 'bar',
+                        version: '0.0.2',
+                        dependencies: {}
+                    }
+                },
+                ranges: {'foo': '>= 0.0.2'},
+                sources: [],
+                current_version: '0.0.2'
+            }
+        });
+        test.done(err);
+    });
+};
+
+exports['build - check only as many sources as needed'] = function (test) {
+    test.expect(3);
+    var source_calls = [];
+    var foo = {
+        name: 'foo',
+        version: '0.0.1',
+        dependencies: {
+            'bar': '>= 0.0.2'
+        }
+    };
+    var sources = [
+        function (name, callback) {
+            test.equal(name, 'bar');
+            source_calls.push('one');
+            process.nextTick(function () {
+                callback(null, {
+                    '0.0.1': {
+                        name: 'bar',
+                        version: '0.0.1',
+                        dependencies: {}
+                    },
+                    '0.0.2': {
+                        name: 'bar',
+                        version: '0.0.2',
+                        dependencies: {}
+                    }
+                });
+            })
+        },
+        function (name, callback) {
+            test.equal(name, 'bar');
+            source_calls.push('two');
+            process.nextTick(function () {
+                callback(null, {
+                    '0.0.2': {
+                        name: 'bar',
+                        version: '0.0.2',
+                        dependencies: {}
+                    },
+                    '0.0.3': {
+                        name: 'bar',
+                        version: '0.0.3',
+                        dependencies: {}
+                    }
+                });
+            })
+        }
+    ];
+    tree.build(foo, sources, function (err, packages) {
+        test.same(source_calls, ['one']);
+        test.same(packages, {
+            'foo': {
+                versions: {
+                    '0.0.1': {
+                        name: 'foo',
+                        version: '0.0.1',
+                        dependencies: {
+                            'bar': '>= 0.0.2'
+                        }
+                    }
+                },
+                ranges: {},
+                sources: [],
+                current_version: '0.0.1'
+            },
+            'bar': {
+                versions: {
+                    '0.0.1': {
+                        name: 'bar',
+                        version: '0.0.1',
+                        dependencies: {}
+                    },
+                    '0.0.2': {
+                        name: 'bar',
+                        version: '0.0.2',
+                        dependencies: {}
+                    }
+                },
+                ranges: {'foo': '>= 0.0.2'},
+                sources: [sources[1]],
+                current_version: '0.0.2'
+            }
+        });
+        test.done(err);
+    });
 };
